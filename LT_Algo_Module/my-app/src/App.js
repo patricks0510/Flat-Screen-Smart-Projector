4f1b81587bfd0271a5408abe910b1cf6a4a1c89d
Pat <59577400+patricks0510@users.noreply.github.com>
2022-06-13 07:34:13 -0400

    First Checkoff Push
    Pushing of demo code for Checkoff 1
    
    This is a very quick and dirty setup to get the idea across for demonstration purposes. Many of the different pieces aren't actually communicating yet.

--- LT_Algo_Module/my-app/src/App.js
+++ LT_Algo_Module/my-app/src/App.js
@@ -1,9 +1,31 @@
 import React from 'react'
+//import SmartTextInput from './SmartTextInput'
+const { ipcRenderer } = window.require('electron')
 
-const App = () => (
-  <div>
-    <h1>Hello React</h1>
-  </div>
-)
+// const App = () => (
+//   <div>
+//     <h1>Hello React</h1>
+//   </div>
+// )
 
-export default App
+export default class App extends React.Component{
+  constructor (props) {
+    super(props)
+    this.state = {ready: false}
+  }
+  componentDidMount () {
+    ipcRenderer.on('fetchData',(event,arg) => {
+      this.setState({ready: arg.ready})
+    })
+    this.intervalId = setInterval(() => { ipcRenderer.send('requestData') }, 50)
+  }
+
+  componentWillUnmount () {
+    ipcRenderer.removeAllListeners('fetchData')
+  }
+
+  render() { return <div>
+    <h1></h1>
+  </div>}
+}
+
