a31cee447d40c94e7af87709a6fb959bf00dc2ca
Pat <59577400+patricks0510@users.noreply.github.com>
2022-06-26 19:01:51 -0400

    Plane Calculations, making LT matrices from that plane
    Plane class created. Takes in 3 vectors and calculates the equation of that plane. Those vectors are then used as unit vectors and a linear transformation matrix can be calculated

--- LT_Algo_Module/my-app/src/App.js
+++ LT_Algo_Module/my-app/src/App.js
@@ -25,7 +25,7 @@ export default class App extends React.Component{
   }
 
   render() { return <div>
-    <h1></h1>
+    <h1>Triangle Shear</h1>
   </div>}
 }
 
--- LT_Algo_Module/my-app/src/main.js
+++ LT_Algo_Module/my-app/src/main.js
@@ -56,7 +56,7 @@ app.on('activate', () => {
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
 
-class Vector {
+class Vector2 {
   constructor(x,y){
     this.x = x
     this.y = y
@@ -67,6 +67,13 @@ class Vector {
 
     this.x = newx
     this.y = newy
+  }
+}
+class Vector3 {
+  constructor(x,y,z){
+    this.x = x
+    this.y = y
+    this.z = z
   }
 }
 /*2x2 matrix class in the form of:
@@ -94,6 +101,72 @@ class Matrix2x2 {
     this.d = this.d.toFixed(3)
   }
 }
+
+//plane class, takes in 3 vectors and calculates the plane
+//v1 is i hat, v2 is j hat, v3 is used to determine z hat
+class Plane {
+  constructor(v1,v2,v3) {
+    this.v1 = v1
+    this.v2 = v2
+    this.v3 = v3
+    var equation = [0,0,0,0]//ax+by+cz=d
+  }
+  calcEq(){
+    //vectors in the plane
+    let vp1 = new Vector3(this.v1.x-this.v2.x,this.v1.y-this.v2.y,this.v1.z-this.v2.z)
+    let vp2 = new Vector3(this.v1.x-this.v3.x,this.v1.y-this.v3.y,this.v1.z-this.v3.z)
+    //cross product of vectors in the plane
+    let xProd = new Vector3(vp1.y*vp2.z-vp1.z*vp2.y,-1*(vp1.x*vp2.z-vp1.z*vp2.x),vp1.x*vp2.y-vp1.y*vp2.x)
+    //create plane equation, RHS being the constant on the right hand side
+    let RHS = this.v1.x*xProd.x + this.v1.y*xProd.y + this.v1.z*xProd.z
+    //equation is the cross product <x,y,z> = RHS
+    this.equation = [xProd.x,xProd.y,xProd.z,RHS]
+  }
+
+  getIHat(){
+    let iHat = this.v1
+    return iHat 
+  }
+
+  getJHat(){
+    let jHat = this.v2
+    return jHat
+  }
+  
+}
+
+var vec1 = new Vector3(4,-3,1)
+var vec2 = new Vector3(-3,-1,1)
+var vec3 = new Vector3(4,-2,8)
+var projPlane = new Plane(vec1,vec2,vec3)
+projPlane.calcEq()
+var lT = new Matrix2x2(projPlane.v1.x,projPlane.v2.x,projPlane.v1.y,projPlane.v2.y)
+console.log(lT)
+lT.invert()
+console.log(lT)
+var newVec1 = new Vector2(vec1.x,vec1.y)
+var newVec2 = new Vector2(vec2.x,vec2.y)
+var newVec3 = new Vector2(vec3.x,vec3.y)
+newVec1.matMult(lT.a,lT.b,lT.c,lT.d)
+newVec2.matMult(lT.a,lT.b,lT.c,lT.d)
+newVec3.matMult(lT.a,lT.b,lT.c,lT.d)
+console.log(newVec1)
+console.log(newVec2)
+console.log(newVec3)
+
+lT.invert()
+newVec1.matMult(lT.a,lT.b,lT.c,lT.d)
+newVec2.matMult(lT.a,lT.b,lT.c,lT.d)
+newVec3.matMult(lT.a,lT.b,lT.c,lT.d)
+console.log(newVec1)
+console.log(newVec2)
+console.log(newVec3)
+
+
+
+
+
+/*
 setTimeout(() => {
   var testMat = new Matrix2x2(4,7,2,6)
   console.log('Original: ')
@@ -106,7 +179,7 @@ setTimeout(() => {
   console.log('Orignial: ')
   console.log(testMat)
   
-  var testVect = new Vector(3,4)
+  var testVect = new Vector3(3,4)
   console.log('Example Coordinates: ')
   console.log(testVect)
   console.log('Applying Transform Matrix')
@@ -114,15 +187,15 @@ setTimeout(() => {
   console.log('Modified Coordinates: ')
   console.log(testVect)
   }, 5000);
-
-setTimeout(() => {
+*/
+/*setTimeout(() => {
   var shear = new Matrix2x2(1,1,0,1)
   console.log('Applied shearing matrix:')
   console.log(shear)
 
-  var start = new Vector(0,-25)
-  var v1 = new Vector(25,0)
-  var v2 = new Vector(25,-25)
+  var start = new Vector3(0,-25)
+  var v1 = new Vector3(25,0)
+  var v2 = new Vector3(25,-25)
   console.log('Original coordinates of the triangle vertices: ' )
   console.log(start)
   console.log(v1)
@@ -138,5 +211,5 @@ setTimeout(() => {
 
   },5000);
 
-
+*/
 
