ea8b6f9e1ae8cb369e70eb0587fde56aaa142cc3
Liam <liamnearyy@gmail.com>
2022-07-30 16:45:16 -0400

    file with invalid bmp declaration (pre-jimp switch)

--- fssp_app/src/ImageTabComponent.tsx
+++ fssp_app/src/ImageTabComponent.tsx
@@ -1,4 +1,3 @@
-import { blob } from "node:stream/consumers";
 import React from "react";
 import { Frame } from "./Frame";
 import './ImageTabComponent.scss';
@@ -13,6 +12,8 @@ interface ImageTabState {
     imageUploaded: boolean;
     originalImage: Blob | null;
     transformedImage: Blob | null;
+    transformedStream: any;
+    bmpbuffer: any;
     imageProjecting: boolean;
 }
 
@@ -23,7 +24,9 @@ export class ImageTabComponent extends React.Component<ImageTabProps, ImageTabSt
             imageUploaded : false,
             originalImage: null,
             transformedImage: null,
-            imageProjecting: false
+            transformedStream: null,
+            imageProjecting: false,
+            bmpbuffer: new ArrayBuffer(0)
         };
         
     }
@@ -39,51 +42,61 @@ export class ImageTabComponent extends React.Component<ImageTabProps, ImageTabSt
                 if (orig_div){
                     orig_div.src = URL.createObjectURL(this.state.originalImage!);
                 }
-                this.setState({transformedImage: this.transformImage()}, () => {
-                    const trans_div = document.getElementById("trans_img") as HTMLImageElement;
-                    if (trans_div){
-                        trans_div.src = URL.createObjectURL(this.state.transformedImage!);
-                    }
-                });
+                this.transformImage();
                 
             });
         }
     }
 
+    convertBufferToUint8 = (arrayBuffer: ArrayBuffer): Uint8Array => {
+        let temp = new Uint8Array();
+        for(let i = 0; i < 1; i++){
+
+        }
+        return temp;
+    }
+
     transformImage = () => {
-        /*var bmp = require("bmp-js");
+        var bmp = require("bmp-js");
+        var bufferReader = new FileReader();
+        bufferReader.addEventListener('loadend', () => {
+            let temp = bufferReader.result as string;
+            this.setState({bmpbuffer: temp}, () => {
+                var trans_blob;
+                console.log(this.state.bmpbuffer);
+                console.log(this.state.bmpbuffer.toString());
+                var bmpData = bmp.decode(this.state.bmpbuffer); 
 
-        class bmpImage {
+            //could have the UI module send a pixel array object, I work with that, and then send a pixel array back
+            let pixelArray = [...bmpData.data]
 
-            bmpData;
-
-            constructor(file: Blob | null){
-                this.bmpData = bmp.decode(file!.arrayBuffer());   
-            }
-        };
-
-        var filename = URL.createObjectURL(this.state.originalImage!);
-        var pic = new bmpImage(this.state.originalImage);
-        //could have the UI module send a pixel array object, I work with that, and then send a pixel array back
-        let pixelArray = [...pic.bmpData.data];
-
-        for(let i = 0; i < pixelArray.length; i+=3){
-            //skip A bit of ARGB
-            i += 1
-            for(let j = 0; j < 3; j++){
-                if(pixelArray[i+j] == 0){
-                    pixelArray[i+j] = 170
+            for(let i = 0; i < pixelArray.length; i+=3){
+                //skip A bit of ARGB
+                i += 1
+                for(let j = 0; j < 3; j++){
+                    if(pixelArray[i+j] === 0){
+                        pixelArray[i+j] = 170
+                    }
                 }
             }
-        }
 
-        pic.bmpData.data = pixelArray;
-        console.log(pic.bmpData);
-        var editImg = bmp.encode(pic.bmpData);
-        console.log(editImg);
+            bmpData.data = pixelArray
+            console.log(bmpData)
+            var editImg = bmp.encode(bmpData)
+            trans_blob = new Blob([editImg], { type: 'application/octet-stream' });
         
-        return editImg;*/
-        return this.state.originalImage;
+            this.setState({transformedStream: editImg, transformedImage: trans_blob}, () => {
+                const trans_div = document.getElementById("trans_img") as HTMLImageElement;
+                    if (trans_div){
+                        trans_div.src = URL.createObjectURL(this.state.transformedImage!);
+                    }
+            });
+            });
+         });
+        
+
+        bufferReader.readAsArrayBuffer(this.state.originalImage!);
+        
     }
 
     toggleProjection = () => {
@@ -115,7 +128,7 @@ export class ImageTabComponent extends React.Component<ImageTabProps, ImageTabSt
             <div className="image-tab">
                 <h1>Image</h1>
                 <Frame className='image-frame-orig'>
-                    <img id="orig_img" className="image"/>   
+                    <img id="orig_img" className="image" alt=""/>   
                 </Frame>
                 <input className="image-upload-btn"
                         accept="image/bmp"
@@ -124,7 +137,7 @@ export class ImageTabComponent extends React.Component<ImageTabProps, ImageTabSt
                     />
                 <h2 className='image-frame-orig caption'>Original Image</h2>
                 <Frame className='image-frame-tran'>
-                    <img id="trans_img" className="image"/>
+                    <img id="trans_img" className="image" alt=""/>
                 </Frame>
                 <button id="proj_button" className='image-project-button' onClick={() => this.toggleProjection()}>Start Projection</button>
                 <h2  className='image-frame-tran caption'>Transformed Image</h2>
